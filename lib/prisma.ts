fe2bbb54e7e1b3bc5d276956762d9178fc083176
Otto Dev <okeldijital@gmail.com>
2026-06-08 09:00:58 +0200

    chore: clean up prisma client initialization

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,10 +5,8 @@ const globalForPrisma = globalThis as unknown as {
 };
 
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log:
-      process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
+  globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
