70cb6102d002ca358212ca3ca062c7df2ae731a9
Otto Dev <okeldijital@gmail.com>
2026-06-08 08:39:14 +0200

    chore: cleanup deprecated files and update config

--- services/officeStatusQuoService.js
+++
@@ -1,9 +0,0 @@
-import api from '../lib/api';
-
-const officeStatusQuoService = {
-    list: (params = {}) => api.get('/office/status-quo', { params }),
-    recompute: () => api.post('/office/status-quo/recompute'),
-    resolve: (id, note) => api.post(`/office/status-quo/${id}/resolve`, { note }),
-};
-
-export default officeStatusQuoService;
--- services/statusQuoService.js
+++
@@ -1,8 +0,0 @@
-import api from '../lib/api';
-
-const statusQuoService = {
-    getDashboard: (params = {}) => api.get('/admin-of-works/status-quo', { params }),
-    recompute: () => api.post('/office/status-quo/recompute'),
-};
-
-export default statusQuoService;
